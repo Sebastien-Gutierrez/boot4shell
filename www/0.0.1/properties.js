ff4ef3106503f08de2c3761e69e180672a769ee6
Sebastien-Gutierrez <sebastien.examios@gmx.fr>
2021-09-25 19:14:53 +0200

    Add files via upload

---
+++ www/0.0.1/properties.js
@@ -0,0 +1,8 @@
+var ed = {
+  partnerId: 1950,
+  applicationName: 'ElephantDrive My Cloud App',
+  nasOSName: 'My Cloud OS',
+  nasVisitEDWebsiteLogin: "http://www.elephantdrive.com/m/ct.aspx?ici=1950",
+  nasVisitEDWebsiteSignup: "http://www.elephantdrive.com/m/ct.aspx?ici=1950",
+  nasVisitEDWebsiteLoggedin: "http://www.elephantdrive.com/m/ct.aspx?ici=1950"
+};
